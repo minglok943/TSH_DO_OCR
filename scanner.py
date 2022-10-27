2876951f1be2dbae4f654bd950a782e04d324dc6
minglok943 <charminglok1026@gmail.com>
2022-10-27 08:21:00 +0800

    created class DO

--- scanner.py
+++ scanner.py
@@ -18,7 +18,7 @@ from dateutil.parser import parse
 
 from regexGen import *
 
-with open('templateDO4.yaml', 'r') as file:
+with open('templateDO6.yaml', 'r') as file:
     companyYaml = yaml.safe_load(file)
 
 def mySqlTable(results, description):
@@ -55,14 +55,8 @@ db = mysql.connect(
         )
 
 myCursor = db.cursor()
-Q1 = "select supplier_do_date, supplier_do_number, po_number, supplier_id\
-        from goodsreceiptsnotes where supplier_do_number='"
 
-Q2 = "select id from purchase_orders where po_number='"
 
-Q3 = "insert into\
-        goodsreceiptsnotes(supplier_do_date, supplier_do_number, po_number, supplier_id)\
-        VALUES (%s,%s,%s,%s)"
 
 cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
 
@@ -84,8 +78,10 @@ class DO:
         self.fieldFound = []
         self.resStr = []
         self.numField = 0
-        self.companyNameFound = False
-        self.companyName = ''
+        self.companyNameFoundFromPO = False
+        self.companyNameFromPO = ''
+        self.companyNameFoundFromYaml = False
+        self.companyNameFromYaml = ''
         self.companyNameRegex =''
         self.itemDetails = [] # item_id, quantity, part_number
         self.itemDetected = [] # item_id, quantity, part_number
@@ -93,18 +89,36 @@ class DO:
         self.startSearchItem = False
         self.do_date = ''
         self.dateFound = False
+        self.companyNameMatched = False
 
-    def searchCompany(self, text):
-        self.companyID = 0 
+    def searchCompanyFromYaml(self, text):
+        self.companyID = 0 # my own index, not same with the database company id
+        for i in companyYaml['companies']:
+            comStr = re.search(i[1], text)
+            if comStr != None:
+                print("Company id ", i[0])
+                print("Company is ", i[2])
+                self.companyNameFromYaml = i[2]
+                self.companyNameFoundFromYaml = True
+                echoStr = 'Company Detected: '+self.companyNameFromYaml
+                secondGnome.echo(echoStr)
+                print(comStr.group())
+                #self.register(self.companyName)
+                #self.cvBoardInitialize(board)
+            else:
+                self.companyID += 1
+
+    def searchCompanyFromPO(self, text):
         poLike = re.search('T-P[0,O]-(\d{8})',text)
         if poLike != None:
-            query = 'select supplier_id from purchase_orders where po_number=\''+poLike.group(1)+'\''
+            query = 'select id, supplier_id from purchase_orders where po_number=\''+poLike.group(1)+'\''
             myCursor.execute(query)
             descr = myCursor.description
             qResults = myCursor.fetchall()
             if qResults:
                 mySqlTable(qResults, descr)
-                self.companyID = qResults[0][0]
+                self.poId = qResults[0][0]
+                self.companyID = qResults[0][1]
                 query = 'select name from suppliers where id='+str(self.companyID)
                 myCursor.execute(query)
                 descr = myCursor.description
@@ -112,7 +126,12 @@ class DO:
                 
                 if qResults:
                     mySqlTable(qResults, descr)
-                    self.companyName = qResults[0][0]
+                    self.companyNameFromPO = qResults[0][0]
+                    echoStr = "company Name From PO num = "+self.companyNameFromPO
+                    secondGnome.echo(echoStr)
+                    self.companyNameFoundFromPO = True
+                    self.po_number = poLike.group(1)
+                    """
                     self.companyNameRegex = genRegex(self.companyName)
 
                     comMatch = re.search(self.companyNameRegex, text)
@@ -125,6 +144,7 @@ class DO:
                         secondGnome.echo(echoStr)
                         self.register(self.companyName)
                         self.cvBoardInitialize(board)
+                    """
 
     def register(self, compName):
         for key, rePattern in companyYaml[compName]['regex'].items():
@@ -142,10 +162,10 @@ class DO:
                     if self.field[i] == 'date':
                         dateStr = find.group(1).replace(" ", "")
                         self.realDate = datetime.strptime(dateStr, \
-                                companyYaml[self.companyName]['date_format']).date()
+                                companyYaml[self.companyNameFromYaml]['date_format']).date()
                         self.resStr[i] = self.realDate.strftime('%Y-%m-%d')
                     elif self.field[i] == 'do_num':
-                        self.resStr[i] = companyYaml[self.companyName]['do_prefix'] + find.group(1)
+                        self.resStr[i] = companyYaml[self.companyNameFromYaml]['do_prefix'] + find.group(1)
                     else:
                         self.resStr[i] = find.group(1)
 
@@ -182,17 +202,22 @@ class DO:
         self.fieldFound.clear()
         self.resStr.clear()
         self.numField = 0
-        self.companyNameFound = False
-        self.companyName = ''
+        self.companyNameFoundFromYaml = False
+        self.companyNameFromYaml = ''
+        self.companyNameFoundFromPO = False
+        self.companyNameFromPO = ''
         self.itemDetails.clear()
         self.itemDetected.clear()
         self.partNumberRegex.clear()
         self.startSearchItem = False
         self.do_date = ''
         self.dateFound = False
+        self.companyNameMatched = False
 
     def checkDatabase(self):
         self.queried = True
+        Q1 = "select id, grn_number, po_number, supplier_id\
+        from goodsreceiptsnotes where supplier_do_number='"
         query = Q1+self.resStr[0]+'\''
         secondGnome.echo(query)
         myCursor.execute(query)
@@ -204,14 +229,19 @@ class DO:
             if len(qResults) > 1:
                 secondGnome.echo('more than one record')
             else:
+                secondGnome.echo('Record Found')
+                self.goodsreceiptsnotesExist = True
+                self.grn_id = qResults[0][0]
                 # check if there is record of DO based on do number
+                """
                 if self.resStr[0] == qResults[0][1]:
-                    secondGnome.echo('Record Found')
-                    secondGnome.echo('Loading item details based on PO ...')
-                    self.loadItemDetails()
-                    self.startSearchItem = True
+                    #self.loadItemDetails()
+                    #self.startSearchItem = True
+                """
         else:
             secondGnome.echo('No record found !!!')
+            self.goodsreceiptsnotesExist = False
+            """
             secondGnome.echo('Press key i to insert, key p to skip')
             key = cv2.waitKey(0)
             if key == ord('i'):
@@ -219,15 +249,63 @@ class DO:
                 self.startSearchItem = True
             else:
                 self.next()
-                board = np.zeros((360,900,3), np.uint8) 
+                board = np.zeros((360,900,3), np.uint8)
+            """
+        secondGnome.echo('Loading item details based on PO ...')
+        self.loadItemDetails()
+        self.startSearchItem = True
 
     def insertDatabase(self):
+
+        if self.goodsreceiptsnotesExist == False:
+            myCursor.execute("select id, grn_number from goodsreceiptsnotes order by grn_number desc limit 1")
+            qResults = myCursor.fetchall()
+            self.grn_id = qResults[0][0]+1
+            self.grn_number = str(int(qResults[0][1])+1)
+            secondGnome.echo("inserting goodsreceiptsnotes")
+            val = (self.grn_number, self.do_date, self.resStr[0],\
+                    self.poId, self.companyID)
+            secondGnome.echo(str(val))
+            Q3 = "insert into\
+            goodsreceiptsnotes(grn_number, supplier_do_date, supplier_do_number,\
+                                po_number, supplier_id)\
+            VALUES (%s,%s,%s,%s,%s)"
+            myCursor.execute(Q3, val)
+            db.commit()
+        else: 
+           secondGnome.echo("goodsreceiptsnotes record exist")
+
+        # itemDetails[0] = purchase_order_items.id
+        # itemDetails[1] = purchase_order_items.item_id
+        # itemDetails[2] = purchase_order_items.quantity
+        # itemDetails[3] = inventories.part_number
+        print("self.itemDetected")
+        print(self.itemDetected)
+        for item in self.itemDetected:
+            query = "select grn_id from goodrecieptsnoteitems where po_item_id='"+str(item[0])+"'"
+            #secondGnome.echo(query)
+            myCursor.execute(query)
+            exist = myCursor.fetchall()
+            #print(exist)
+            if not exist:
+                secondGnome.echo("inserting goodrecieptsnoteitems")
+                Q6 = "insert into goodrecieptsnoteitems(grn_id, po_item_id, item_id, ordered_quantity)\
+                VALUES(%s,%s,%s,%s)"
+                val1 = (self.grn_id, item[0], item[1], item[2])
+                secondGnome.echo(str(val1))
+                myCursor.execute(Q6, val1)
+                db.commit()
+            else:
+                echoStr = item[3]+"with item_id="+str(item[1])+", po_item_id="+str(item[0])+" exist in goodrecieptsnoteitems"
+                secondGnome.echo(echoStr)
+        
+        """
+        Q2 = "select id from purchase_orders where po_number='"
         query = Q2+self.po_number+'\'' # query po_number id 
         secondGnome.echo(query)
         myCursor.execute(query)
         descr = myCursor.description
         qResults = myCursor.fetchall()
-        
         if qResults:
             mySqlTable(qResults, descr)
             if len(qResults) > 1:
@@ -238,17 +316,24 @@ class DO:
                 val = (self.do_date, self.resStr[0],\
                         po_id, self.companyID)
                 print(val)
+                Q3 = "insert into\
+                goodsreceiptsnotes(supplier_do_date, supplier_do_number, po_number, supplier_id)\
+                VALUES (%s,%s,%s,%s)"
                 myCursor.execute(Q3, val)
                 db.commit()
         else:
             secondGnome.echo('No po_number id record found')
+        """
 
     def loadItemDetails(self):
+        # itemDetails[0] = purchase_order_items.id
+        # itemDetails[1] = purchase_order_items.item_id
+        # itemDetails[2] = purchase_order_items.quantity
+        # itemDetails[3] = inventories.part_number
         Q4 = "drop temporary table if exists tempPO;"
-        Q5 = "create temporary table tempPO select poi.item_id, poi.quantity, i.part_number\
+        Q5 = "create temporary table tempPO select poi.id, poi.item_id, poi.quantity, i.part_number\
         from purchase_order_items poi inner join inventories i on poi.item_id=i.id \
         where poi.po_id=(select id from purchase_orders where po_number='"+self.po_number+"');"
-        ## resStr[0] = date, [1]=do_num, [2]=po_number
         Q6 = "select * from tempPO;"
         myCursor.execute(Q4)
         myCursor.execute(Q5)
@@ -267,12 +352,17 @@ class DO:
 
     def searchItem(self, text):
         record = []
+        # itemDetails[0] = purchase_order_items.id
+        # itemDetails[1] = purchase_order_items.item_id
+        # itemDetails[2] = purchase_order_items.quantity
+        # itemDetails[3] = inventories.part_number
         for index, item_detail in enumerate(self.itemDetails):
-            match = re.search(genRegexCapitalInsensitive(self.itemDetails[index][2]), text)
+            match = re.search(genRegexCapitalInsensitive(self.itemDetails[index][3]), text)
             #secondGnome.echo(genRegexCapitalInsensitive(self.itemDetails[index][2]))
             if match != None:
-                echoStr = 'Found item with id '+str(item_detail[0])\
-                    +' \''+item_detail[2]+'\''
+                self.itemDetected.append(item_detail)
+                echoStr = 'Found item with id '+str(item_detail[1])\
+                    +' \''+item_detail[3]+'\''
                 #echoStr = 'Found item with id '+str(detail[index][0])
                 secondGnome.echo(echoStr)
             #score = fuzz.ratio(item_detail[2], text)
@@ -281,8 +371,8 @@ class DO:
             else:
                 record.append(index) ## remaining 
         #if len(record
-        for i in record:
-            self.itemDetected.append(self.itemDetails[i])
+        #for i in record:
+            #self.itemDetected.append(self.itemDetails[i])
             #print("Detected")
             #print(self.itemDetected)
 
@@ -293,9 +383,9 @@ class DO:
 
     def cvBoardInitialize(self, cvMat):
         
-        cvMat = cv2.putText(cvMat, self.companyName, (50, 50), font, 
+        cvMat = cv2.putText(cvMat, self.companyNameFromYaml, (50, 50), font, 
                        fontScale, color, thickness, cv2.LINE_AA)
-        cvMat = cv2.putText(cvMat, self.po_number, (50, 85), font, 
+        cvMat = cv2.putText(cvMat, "T-PO-"+self.po_number, (50, 85), font, 
                fontScale, color, thickness, cv2.LINE_AA)
 
         for i in range(0, self.numField):
@@ -343,6 +433,7 @@ def scan_detection(image):
     cv2.drawContours(frame, [document_contour], -1, (0, 255, 0), 3)
 
 
+echoOnce = False
 while True:
 
     _, frame = cap.read()
@@ -359,28 +450,51 @@ while True:
 
     pressed_key = cv2.waitKey(1) & 0xFF
         
-   # allFound = jobFound+salesFound+termsFound+dateFound+totalFound+invoiceNoFound
-    if (d_o.numFieldFound != d_o.numField and d_o.numField != 0) \
-            or d_o.companyNameFound == False\
-            or d_o.dateFound == False\
-            or d_o.startSearchItem == True:
-        #ocr_text = pytesseract.image_to_string(warped)
+
+    if d_o.companyNameMatched == False:
         ocr_text = pytesseract.image_to_string(frame)
-        if d_o.companyNameFound == False:
-            d_o.searchCompany(ocr_text)
-        elif d_o.startSearchItem == True:
-            d_o.searchItem(ocr_text)
-            if len(d_o.itemDetails) == 0:
-                d_o.startSearchItem = False
-        elif d_o.dateFound == False:
-            d_o.searchDate(ocr_text)
-        else: 
-            d_o.search(ocr_text)
+        if d_o.companyNameFoundFromPO == False:
+            d_o.searchCompanyFromPO(ocr_text)
+        if d_o.companyNameFoundFromYaml == False:
+            d_o.searchCompanyFromYaml(ocr_text)
+
+        if d_o.companyNameFoundFromYaml == True and d_o.companyNameFoundFromPO == True:
+            if d_o.companyNameFromYaml == d_o.companyNameFromPO:
+                d_o.register(d_o.companyNameFromYaml)
+                d_o.cvBoardInitialize(board)
+                d_o.companyNameMatched = True
+                secondGnome.echo("Company Found From PO number matched with DO")
+            else:
+                secondGnome.echo("Company Found from PO number not matched with DO")
+                d_o.companyNameFoundFromYaml = False
+                d_o.companyNameFoundFromPO = False
+                d_o.companyNameFromPO = ''
+                d_o.companyFromYaml = ''
+    else:
+        if (d_o.numFieldFound != d_o.numField and d_o.numField != 0) \
+                or d_o.dateFound == False\
+                or d_o.startSearchItem == True:
+            #ocr_text = pytesseract.image_to_string(warped)
+            ocr_text = pytesseract.image_to_string(frame)
+
+            if d_o.startSearchItem == True:
+                d_o.searchItem(ocr_text)
+                if len(d_o.itemDetails) == 0:
+                    d_o.startSearchItem = False
+            elif d_o.dateFound == False:
+                d_o.searchDate(ocr_text)
+            else: 
+                d_o.search(ocr_text)
+        else:
+            if echoOnce == False:
+                echoOnce = True
+                secondGnome.echo("Everything ready, Press i to insert to Database")
 
     cv2.imshow("Result", board)
     cv2.moveWindow("Result", 650, 0)
     
     if d_o.queried == False and (d_o.numFieldFound == d_o.numField and d_o.numField != 0) and d_o.dateFound == True:
+        secondGnome.echo("checkDatabase")
         d_o.checkDatabase()
     
     #cv2.imshow("Warped", cv2.resize(warped, (int(0.75 * warped.shape[1]), int(0.75 * warped.shape[0]))))
@@ -399,6 +513,9 @@ while True:
     elif pressed_key == ord('n'):
         d_o.next()
         board = np.zeros((360,900,3), np.uint8)
+        secondGnome.send('clear')
+    elif pressed_key == ord('i'):
+        d_o.insertDatabase()
 
 cv2.destroyAllWindows()
 secondGnome.kill()
